c520d58335a827ea89afc6e1b56491134838a538
Sandy Berkowitz <elybarak7589@gmail.com>
2021-08-12 05:16:54 +0300

    misc

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
 class HTMLFetch extends HTMLElement {
-				constructor () {
-								super();
-				}
-				
-				async connectedCallback() {
-								const path = this.getAttribute("path");
-								const headers = this.getAttribute("headers");
-							 const attr = [...this.attributes].map(x => x.name).join(",");
-							 console.log(attr)
-								console.log(path)
-								console.log(headers)
-								try {
-												const resp = await fetch(path, {
-																method: "GET",
-																headers: JSON.parse(headers)
-												});
-								
-												const text = await resp?.text();
-												this.innerHTML = resp?.status + ' ' + text
-								} catch (e) {
-												this.innerHTML = JSON.stringify(e)
-								}
-								
-					}
+  constructor() {
+    super();
+  }
+
+  async connectedCallback() {
+    const path = this.getAttribute("path");
+    const headers = this.getAttribute("headers");
+    const attr = [...this.attributes].map((x) => x.name).join(",");
+    console.log(attr);
+    console.log(path);
+    console.log(headers);
+    try {
+      const resp = await fetch(path, {
+        method: "GET",
+        headers: JSON.parse(headers),
+      });
+
+      const text = await resp?.text();
+      this.innerHTML = resp?.status + " " + text;
+    } catch (e) {
+      this.innerHTML = JSON.stringify(e);
+    }
+  }
 }
 customElements.define("x-fetch", HTMLFetch);
